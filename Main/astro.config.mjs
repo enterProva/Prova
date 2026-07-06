348fb26d2d8a1f867262cce77453c3115f6f4e2f
Suhani Tamang <suhaniyh@gmail.com>
2026-07-06 15:41:08 +0000

    changes

--- Main/astro.config.mjs
+++ Main/astro.config.mjs
@@ -1,5 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel'; // Or another adapter like netlify, node, etc.
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'hybrid',
+  adapter: vercel(),
+});
