2c2bd7b2b506d5d20d6dbd5642d8829f8362685f
Lily Qin <lilyqin527@gmail.com>
2026-07-20 10:03:57 -0400

    rename folder

---
+++ Main/dist/server/chunks/about_BwKrGetq.mjs
@@ -0,0 +1,19 @@
+import { t as __exportAll } from "./rolldown-runtime_BBjsoOtd.mjs";
+import { u as renderTemplate } from "./server_DiftD9Vj.mjs";
+import { t as createComponent } from "./compiler_Ch8emKFc.mjs";
+//#region src/pages/about.astro
+var about_exports = /* @__PURE__ */ __exportAll({
+	default: () => $$About,
+	file: () => $$file,
+	url: () => $$url
+});
+var $$About = createComponent(($$result, $$props, $$slots) => {
+	return renderTemplate``;
+}, "/home/lily/Prova/Main/src/pages/about.astro", void 0);
+var $$file = "/home/lily/Prova/Main/src/pages/about.astro";
+var $$url = "/about";
+//#endregion
+//#region \0virtual:astro:page:src/pages/about@_@astro
+var page = () => about_exports;
+//#endregion
+export { page };
