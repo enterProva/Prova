3118dc73dd4d4d63ab076d45fea7291a9836f6e0
zacHub-alt <lac.zuh@gmail.com>
2026-08-16 19:27:22 +0100

    Update App.tsx

--- main-app/client/src/App.tsx
+++ main-app/client/src/App.tsx
@@ -51,6 +51,7 @@ export default function App() {
     <QueryClientProvider client={queryClient}>
       {showIntroSplash ? <SplashScreen /> : <Router />}
     <Analytics/>
+    </>
     </QueryClientProvider>
   );
 }
