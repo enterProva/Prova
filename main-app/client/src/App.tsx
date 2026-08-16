3f0d440ee89af1b66d1ff6778d33af28408d5787
zac <lac.zuh@gmail.com>
2026-08-16 20:32:39 +0100

    Add vercel analytics, fix JSX fragment

--- main-app/client/src/App.tsx
+++ main-app/client/src/App.tsx
@@ -51,7 +51,6 @@ export default function App() {
     <QueryClientProvider client={queryClient}>
       {showIntroSplash ? <SplashScreen /> : <Router />}
     <Analytics/>
-    </>
     </QueryClientProvider>
   );
 }
